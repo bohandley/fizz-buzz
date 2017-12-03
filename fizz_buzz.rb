0af3776151d9c52f9eb260a63fc5c41ad155a4ba
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-12-03 09:27:01 -0600

    add error handling for args values not all integers

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -8,6 +8,7 @@
 # with error handling for total < 1
 
 def fizz_buzz_with_care(args)
+  raise(ArgumentError, 'All values of args must be integers') if !args.values.all? {|i| i.class == Integer}
   range = ''
   1.upto(args[:total]) do |i|
     fb = ''
@@ -27,73 +28,6 @@ def is_multiple(num, mult)
 end
 
 values = { int1: 2, int2: 3, total: 24 }
+values1 = { int1: 2, int2: 3, total: "24" }
 puts fizz_buzz_with_care(values)
-
-
-# with single responsibility methods
-
-# def fizz_buzz_methods(int1, int2, total)
-#   1.upto(total) do |i|
-#     fb = ''
-#     if is_multiple(i, int1) || is_multiple(i, int2)
-#       fb += 'fizz' if is_multiple(i, int1)
-#       fb += 'buzz' if is_multiple(i, int2)
-#       puts fb
-#     else
-#       puts i
-#     end
-#   end
-# end
-
-# def is_multiple(num, mult) 
-#   num % mult == 0
-# end
-
-# fizz_buzz_methods(2,3,20)
-# # With a loop
-# def fizz_buzz_loop(total)
-#   1.upto(total) do |i|
-#     if i % 5 == 0 || i % 3 ==0
-#       puts 'fizzbuzz' if i % 5 == 0 && i % 3 ==0
-#       puts 'buzz' if i % 5 == 0 && i % 3 != 0
-#       puts 'fizz' if i % 3 == 0 && i % 5 != 0
-#     else
-#       puts i
-#     end
-#   end
-# end  
-
-# fizz_buzz_loop(16)
-
-# # incrementing a string
-# def fizz_buzz_string(total)
-#   1.upto(total) do |i|
-#     fb = ''
-#     if i % 5 == 0 || i % 3 ==0
-#       fb += 'fizz' if i % 3 == 0
-#       fb += 'buzz' if i % 5 == 0
-#       puts fb
-#     else
-#       puts i
-#     end
-#   end
-# end
-
-# fizz_buzz_string(16)
-
-# # passing integer values
-# def fizz_buzz_val(int1, int2, total)
-#   1.upto(total) do |i|
-#     fb = ''
-#     if i % int1 ==0 || i % int2 == 0 
-#       fb += 'fizz' if i % int1 == 0
-#       fb += 'buzz' if i % int2 == 0
-#       puts fb
-#     else
-#       puts i
-#     end
-#   end
-# end
-
-# fizz_buzz_val(2,6,100)
-
+puts fizz_buzz_with_care(values1)
