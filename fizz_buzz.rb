cd5ba00dd58a6cc120baf41e2f1546b280113cb1
Brendan O'Handley <brendan.ohandley@gmail.com>
2017-12-03 08:49:35 -0600

    pseudocode for error handling, and returning a string instead of printing in the method.

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,3 +1,35 @@
+# input = int1 is an integer, int2 is an integer, total is an integer
+# output = string containing every number from 1 to total where
+  # multiples of int1 are replaced by 'fizz'
+  # multiples of int2 are replaced by 'buzz'
+  # multiples of both int1 and int2 are replaced by 'fizzbuzz' 
+# with error handling for argument order
+# with error handling for arguments not integers
+# with error handling for total < 1
+
+
+
+
+# with single responsibility methods
+
+def fizz_buzz_methods(int1, int2, total)
+  1.upto(total) do |i|
+    fb = ''
+    if is_multiple(i, int1) || is_multiple(i, int2)
+      fb += 'fizz' if is_multiple(i, int1)
+      fb += 'buzz' if is_multiple(i, int2)
+      puts fb
+    else
+      puts i
+    end
+  end
+end
+
+def is_multiple(num, mult) 
+  num % mult == 0
+end
+
+fizz_buzz_methods(2,3,20)
 # With a loop
 def fizz_buzz_loop(total)
   1.upto(total) do |i|
@@ -45,23 +77,3 @@ end
 
 fizz_buzz_val(2,6,100)
 
-# with single responsibility methods
-
-def fizz_buzz_methods(int1, int2, total)
-  1.upto(total) do |i|
-    fb = ''
-    if is_multiple(i, int1) || is_multiple(i, int2)
-      fb += 'fizz' if is_multiple(i, int1)
-      fb += 'buzz' if is_multiple(i, int2)
-      puts fb
-    else
-      puts i
-    end
-  end
-end
-
-def is_multiple(num, mult) 
-  num % mult == 0
-end
-
-fizz_buzz_methods(2,3,20)
